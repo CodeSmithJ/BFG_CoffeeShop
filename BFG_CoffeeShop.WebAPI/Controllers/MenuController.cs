f3c86b1bb7414fb4e0b9aea221c68cf5128bf889
amckasson90@gmail.com <amckasson90@gmail.com>
2021-06-07 16:55:51 -0400

    Last Controller Commit

--- BFG_CoffeeShop.WebAPI/Controllers/MenuController.cs
+++ BFG_CoffeeShop.WebAPI/Controllers/MenuController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BFG_CoffeeShop.Models.MenuModels;
+using BFG_CoffeeShop.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,63 @@ namespace BFG_CoffeeShop.WebAPI.Controllers
 {
     public class MenuController : ApiController
     {
+        private MenuService _menuService = new MenuService();
+
+        public IHttpActionResult Get()
+        {
+            MenuService menuService = CreateMenuService();
+            var menus = menuService.GetMenus();
+            return Ok(menus);
+        }
+
+        public IHttpActionResult Post(MenuCreate menu)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateMenuService();
+
+            if (!service.CreateMenu(menu))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private MenuService CreateMenuService()
+        {
+            //var menuId = User.Identity.Get
+            var menuService = new MenuService();
+            return menuService;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            MenuService menuService = CreateMenuService();
+            var menu = menuService.GetMenuById(id);
+            return Ok(menu);
+        }
+
+        public IHttpActionResult Put(MenuEdit menu)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateMenuService();
+
+            if (!service.UpdateMenu(menu))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateMenuService();
+
+            if (!service.DeleteMenu(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
