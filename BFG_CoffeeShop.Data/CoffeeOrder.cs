6b3db293346701f170c8ce26cd682d3436d6a2d5
Chris Kronberg <ckronber@gmail.com>
2021-06-07 13:52:51 -0400

    CoffeeOrder Class Created

---
+++ BFG_CoffeeShop.Data/CoffeeOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFG_CoffeeShop.Data
+{
+    public class CoffeeOrder
+    {
+        [Key]
+        public int CoffeeOrderId {get; set;}
+        public string FullName {get; set;}
+        public string Country_Of_Origin { get; set; }
+        public double TotalPrice { get; set; }
+        public string Barista { get; set; }
+        public int AdditionId {get; set;}
+        //[ForeignKey(nameof(AdditionId))]
+        //List<Addition> Additions { get; set; }
+        public int CustomerId { get; set; }
+        //[ForeignKey(nameof(CustomerId))]
+        //List<Customer> Customers { get; set; }        
+    }
+}
