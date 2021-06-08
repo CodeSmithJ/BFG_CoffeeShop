95c7e454658d974d0f8c5d4b228a03f04b6cf1da
Jesse Smith <worldjesses@gmail.com>
2021-06-08 11:20:04 -0400

    Stole Chris's Not Corrupt Data

---
+++ BFG_CoffeeShop.Data/CoffeeShop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFG_CoffeeShop.Data
+{
+    public class CoffeeOrder
+    {
+        [Key]
+        public int CoffeeOrderId { get; set; }
+        public string FullName { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Edited { get; set; }
+        public string Country { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Barista { get; set; }
+        //List<Addition> Additions { get; set; }
+
+        //List<Customer> Customers { get; set; }
+
+        //List<Menu> Menus { get; set; }  
+    }
+}
